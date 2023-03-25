7f0c976f77dfff35615a9963c75ee9df3db5237b
Raphael Riet <112032709+raphaelriet@users.noreply.github.com>
2023-03-24 22:02:37 -0300

    Update ex0.1.2.js

--- ex0.1.2.js
+++ ex0.1.2.js
@@ -1,26 +1,23 @@
 function limiteDesc(desc) {
-   if(desc > 100){
-    desc = 100;
-   }
-   return desc;
+    if (desc > 100) {
+        desc = 100;
+    }
+    return desc;
 }
 
-function cliente(valor, convenio, cartao){
-    if (cartao && convenio){
+function cliente(valor, convenio, cartao) {
+    if (cartao && convenio) {
         let desc = valor * 0.15;
         valor = valor - limiteDesc(desc);
-        console.log('você possui desconto de 15%')
+        console.log('você possui desconto de 15% limitado em R$100,00')
         return valor;
-    } else if (convenio || cartao){
+    } else if (convenio || cartao) {
         let desc = valor * 0.10;
         valor = valor - limiteDesc(desc);
-        console.log('você possui desconto de 10%')
-        return valor;
-    } else {
-        console.log('você não possui desconto')
+        console.log('você possui desconto de 10% limitado em R$100,00')
         return valor;
     }
 }
 
-    const teste = cliente(1500, true, false);
-    console.log(teste);
+const teste = cliente(1500, true, false);
+console.log(teste);
